18fc4eefd8d638a571577f59e3083a489714389e
BeiZero <fedorov.igor95@gmail.com>
2018-07-26 17:28:35 +0400

    Fix

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-find ./lectures -iname "*.md"  -mindepth 2 -maxdepth 2 -type f -exec sh -c 'pandoc -t revealjs -s -o "./docs/$(basename ${0%.md}.html)" "${0}" -V revealjs-url=' {} \;
+find ./lectures -iname "*.md"  -mindepth 2 -maxdepth 2 -type f -exec sh -c 'pandoc -t revealjs -s -o "./docs/$(basename ${0%.md}.html)" "${0}" -V revealjs-url=https://macs-club.github.io/agda-lectures' {} \;
